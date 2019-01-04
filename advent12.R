a063da0fc4015b90303dd355fac18eac4664c6e2
Edwin Thoen <edwinthoen@fre-14-027.funda.nl>
2019-01-04 08:58:30 +0100

    solved 12.1

--- advent12.R
+++ advent12.R
@@ -11,10 +11,10 @@ growth_df <- growth %>%
 
 ## Part 1
 add_empty_pots <- function(state) {
-  pad_before <- rep(".", 2)
-  names(pad_before) <- as.numeric(min(names(state))) - 2:1
-  pad_after <- rep(".", 2)
-  names(pad_after) <- as.numeric(max(names(state))) + 1:2
+  pad_before <- rep(".", 3)
+  names(pad_before) <- as.numeric(min(names(state) %>% as.numeric)) - 3:1
+  pad_after <- rep(".", 4)
+  names(pad_after) <- as.numeric(max(names(state) %>% as.numeric)) + 1:4
   c(pad_before, state, pad_after)
 } 
 
@@ -22,12 +22,14 @@ get_next_generation <- function(state) {
   pots_with_adjacent <- map_df(names(state) %>% as.numeric, function(ind) {
     all_ind <- (ind - 2):(ind + 2)
     paste0(state[names(state) %in% all_ind], collapse = "") %>% 
-      data_frame(this_generation = .)
-  }) 
+      data_frame(ind = ind, this_generation = .)
+  }) %>% filter(nchar(this_generation) == 5) 
+  
   new_generation <- 
-    inner_join(pots_with_adjacent, growth_df, by = c("this_generation" = "state")) %>% 
+    left_join(pots_with_adjacent, growth_df, by = c("this_generation" = "state")) %>% 
     pull(new)
-  names(new_generation) <- -1:(length(new_generation) - 2)
+  new_generation[is.na(new_generation)] <- "."
+  names(new_generation) <- pots_with_adjacent$ind
   new_generation
 }
 
