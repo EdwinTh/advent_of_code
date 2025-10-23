2b0b4d5b7ab7f1d93e821d25413f5fcd9e501fe0
Edwin Thoen <edwinthoen@gmail.com>
2025-10-23 15:53:49 +0200

    solved day 7

--- AoC_2024/day_07/script07.py
+++ AoC_2024/day_07/script07.py
@@ -1,11 +1,67 @@
-from itertools import combinations_with_replacement, permutations
-with open('data07_test.txt') as d:
+from itertools import product
+with open('data07.txt') as d:
     data = [el.strip() for el in d.readlines()]
 
-class Combination:
+class Evaluation:
     def __init__(self, data):
         self.test_value = int(data.split(':')[0])
         self.values = [int(v) for v in data.split(':')[1].strip().split(" ")]
+        self.n = len(self.values)
 
     def _get_all_operators(self):
-        len(self.values)
+        self.operators = product('+*', repeat = self.n - 1)
+
+    def _eval_operator(self, operator):
+        value = 0
+        operator = ['+'] + [o for o in operator] 
+        for i in range(self.n):
+            if operator[i] == '+':
+                value += self.values[i]
+            else:
+                value *= self.values[i]
+        return value
+
+    def return_value(self):
+        for operator in self.operators:
+            if self._eval_operator(operator) == self.test_value:
+                return self.test_value
+        return 0
+
+    def evaluate(self):
+        self._get_all_operators()
+        return self.return_value()
+
+print("Star 1:", sum([Evaluation(d).evaluate() for d in data]))
+
+class Evaluation2:
+    def __init__(self, data):
+        self.test_value = int(data.split(':')[0])
+        self.values = [int(v) for v in data.split(':')[1].strip().split(" ")]
+        self.n = len(self.values)
+
+    def _get_all_operators(self):
+        self.operators = product('+*|', repeat = self.n - 1)
+
+    def _eval_operator(self, operator):
+        value = 0
+        operator = ['+'] + [o for o in operator] 
+        for i in range(self.n):
+            if operator[i] == '+':
+                value += self.values[i]
+            elif operator[i] == '*':
+                value *= self.values[i]
+            else:
+                value = int(str(value) + str(self.values[i]))
+        return value
+
+    def return_value(self):
+        for operator in self.operators:
+            if self._eval_operator(operator) == self.test_value:
+                return self.test_value
+        return 0
+
+    def evaluate(self):
+        self._get_all_operators()
+        return self.return_value()
+
+print("Star 2:", sum([Evaluation2(d).evaluate() for d in data]))
